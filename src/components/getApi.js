5ea791177ab466f861d24f7bcec4b128176d9ed6
Soni Nishit <nishitsoni15>
2022-01-30 23:18:52 +0530

    post put delete api commit

--- src/components/getApi.js
+++ src/components/getApi.js
@@ -8,25 +8,43 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { getByPlaceholderText } from '@testing-library/dom';
+import { ListItem } from '@mui/material';
 
 const GetApi = () => {
     const[newData,setNewData] = useState([]);
+    const[open,setOpen] = useState(false);
+    const[id,setId] = useState('');
+    const[username,setUserName] = useState('');
+    const[place,setPlace] = useState('');
+    const[contact,setContact] = useState('');
     useEffect(() =>{
+        getData();
+    },[])
+    console.log('resp value',newData)
+    function getData(){
         axios.get('http://localhost:3001/Profiles')
         .then((response)=>{
         setNewData(response.data)
+        // setId(response[0].id)
+        // setUserName(response[0].username)
+        // setPlace(response[0].place)
+        // setContact(response[0].contact)     
     })
     .catch(error => {
     console.log(error);
     })
-    },[])
-    console.log('resp value',newData)
-
-    function createData(id,name,place,contact) {
-        return { id, name, place, contact};
-      }
+    }
+    // function createData(id,name,place,contact) {
+    //     return { id, name, place, contact};
+    //   }
       
       const rows = [
         // createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
@@ -35,6 +53,56 @@ const GetApi = () => {
         // createData('Cupcake', 305, 3.7, 67, 4.3),
         // createData('Gingerbread', 356, 16.0, 49, 3.9),
       ];
+    
+
+      const createModal = (id,e) => {
+          e.preventDefault();
+          setOpen(true)
+        console.log('same value', newData[id-1])
+          let item = newData[id-1]
+          setId(item.id)
+        setUserName(item.username)
+        setPlace(item.place)
+        setContact(item.contact)
+      }
+
+      const handleClose = () => {
+          setOpen(false)
+      }
+
+      function deleteEntry(id,e) {
+          e.preventDefault();
+        axios.delete(`http://localhost:3001/Profiles/${id}`)
+        .then((resp) => {
+            console.log(resp)
+        })
+        .catch((error) => {
+            console.log(error)
+        })  
+        getData();   
+  }
+
+    const editUSer = () => {
+        let item1 = {id,username,place,contact}
+        fetch(`http://localhost:3001/Profiles/${id}`,{
+            method:'PUT',
+            headers:{
+                'Accept': 'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(item1)
+        })
+        .then((result) => {
+            result.json().then((resp) => {
+                console.log(resp.data)
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })  
+        getData();  
+        setOpen(false);
+    }
       
     return(
         <>
@@ -43,7 +111,7 @@ const GetApi = () => {
               <TableHead>
                 <TableRow>
                   <TableCell align="center">id</TableCell>
-                  <TableCell align="center">name</TableCell>
+                  <TableCell align="center">username</TableCell>
                   <TableCell align="center">place</TableCell>
                   <TableCell align="center">contact</TableCell>
                   <TableCell align="center">actions</TableCell>
@@ -52,22 +120,75 @@ const GetApi = () => {
               <TableBody>
                 {newData.map((row) => (
                   <TableRow
-                    key={row.name}
+                    key={row.username}
                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                   >
                     {/* <TableCell component="th" scope="row">
                       {row.name}
                     </TableCell> */}
                     <TableCell align="center">{row.id}</TableCell>
-                    <TableCell align="center">{row.name}</TableCell>
+                    <TableCell align="center">{row.username}</TableCell>
                     <TableCell align="center">{row.place}</TableCell>
                     <TableCell align="center">{row.contact}</TableCell>
-                    <TableCell align="center">{<Button variant="contained">Contained</Button> }{<Button variant="outlined">Outlined</Button>}</TableCell>
+                    <TableCell align="center">{<Button variant="contained" onClick={(e) => createModal(row.id,e)}>Edit</Button> } &nbsp;{<Button variant="outlined" onClick={(e) => deleteEntry(row.id,e)}>Delete</Button>}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
             </Table>
           </TableContainer>
+          <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Edit Profile</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="id"
+            label="Id"
+            type="number"
+            fullWidth
+            variant="standard"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+           <TextField
+            autoFocus
+            margin="dense"
+            id="username"
+            label="Userame"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={username}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+           <TextField
+            autoFocus
+            margin="dense"
+            id="place"
+            label="Place"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={place}
+            onChange={(e) => setPlace(e.target.value)}
+          />
+           <TextField
+            autoFocus
+            margin="dense"
+            id="contact"
+            label="Contact"
+            type="number"
+            fullWidth
+            variant="standard"
+            value={contact}
+            onChange={(e) => setContact(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={editUSer}>Edit</Button>
+        </DialogActions>
+      </Dialog>
         </>
     )
 }
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,17 +3,32 @@ import { ButtonComponent, Counter } from "../components/index";
 import react from "react";
 import { useNavigate } from 'react-router-dom';
 import { GetApi } from '../components/index';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import axios from "axios";
 
 
 class Dashboard extends react.Component{
     // navigate = useNavigate()
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
         this.state={
             text:"count",
-            count:0
-        }
+            count:0,
+            show:false,
+            id:'',
+            username:'',
+            place:'',
+            contact:''
+        };
     }
+
     IncrementFunc(){
         setInterval(() => {
             this.setState({count:this.state.count+1})
@@ -44,12 +59,45 @@ class Dashboard extends react.Component{
        })
     }
 
+    createProfile = () => {
+        this.setState(
+            {
+                show:true
+            }
+        )
+    }
+    handleClose = () => {
+        this.setState({
+            show:false
+        })
+    }
+
+    handleChange = (e) => {
+        this.setState({
+         [e.target.id]:e.target.value,
+         [e.target.username]:e.target.value,
+         [e.target.place]:e.target.value,
+         [e.target.contact]:e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+            axios.post('http://localhost:3001/Profiles',this.state)
+        .then((response)=>{
+        console.log(response);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+        this.setState({
+            show:false
+        })
+    }
 
     render(){
-    
         return(
             <>
-            
             <Header/>
             <h1>Welcome to Dashboard!</h1>
             <div>{this.state.text}</div>
@@ -68,9 +116,64 @@ class Dashboard extends react.Component{
             <br/><br />
             {/* <CountAuto/> */}
             <br></br>
+            <Button variant="contained" onClick={this.createProfile}>Create Profile</Button>
+            <br/><br />
+            <Dialog open={this.state.show} onClose={this.handleClose}>
+        <DialogTitle>Add Profile</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="id"
+            label="Id"
+            type="number"
+            fullWidth
+            variant="standard"
+            value={this.state.id}
+            onChange={this.handleChange}
+          />
+           <TextField
+            autoFocus
+            margin="dense"
+            id="username"
+            label="Username"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+           <TextField
+            autoFocus
+            margin="dense"
+            id="place"
+            label="Place"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={this.state.place}
+            onChange={this.handleChange}
+          />
+           <TextField
+            autoFocus
+            margin="dense"
+            id="contact"
+            label="Contact"
+            type="number"
+            fullWidth
+            variant="standard"
+            value={this.state.contact}
+            onChange={this.handleChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => this.handleClose()}>Cancel</Button>
+          <Button onClick={this.handleSubmit}>Add</Button>
+        </DialogActions>
+      </Dialog>
+      <br/><br/>
             <GetApi />
             </>
-
         )
     }
 }
